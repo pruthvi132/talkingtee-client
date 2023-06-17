c2910a31ee1e299d5bdb77c2c6cee4dfad8b59ad
pruthvi132 <pruthviappu922@gmail.com>
2023-06-17 18:42:49 +0530

    added header

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,15 +7,3 @@ import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 
 export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
-fetch(
-  "https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/potsdamer-platz/potsdamer_platz_1k.hdr",
-  {
-    mode: "no-cors",
-  }
-)
-  .then((response) => {
-    // Handle the response here
-  })
-  .catch((error) => {
-    // Handle any errors here
-  });
